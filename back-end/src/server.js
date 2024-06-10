f5b774b2150fdcf3080f0585a7191189768e79d8
jasongreenb <greenberg.jason2004@gmail.com>
2024-06-10 15:49:21 -0400

    Changed to express promise router in routes, hid the file from git, seperated the functionality too

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,17 +5,16 @@ const mysql = require("mysql2");
 const parseCSV = require("./fileParser");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 require("dotenv").config();
+const server = express();
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(cors());
 
 async function handleParse(file) {
   const results = [];
   await parseCSV(file, results);
   return results;
 }
-
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-server.use(cors());
 
 const db = mysql.createConnection({
   host: process.env.host,
@@ -33,6 +32,23 @@ const s3Client = new S3Client({
   },
 });
 
+const upload = multer({ storage: multer.memoryStorage() });
+
+async function uploadToB2(file) {
+  const uploadParams = {
+    Bucket: process.env.bucket,
+    Key: `${Date.now()}_${file.originalname}`, // Unique file name
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  await s3Client.send(command);
+}
+
+const uploadRoute = require("./routes/file_upload_routes/upload_post_route");
+server.use("/", uploadRoute({ handleParse, db, s3Client, upload, uploadToB2 }));
+
 db.connect((e) => {
   if (e) {
     console.error("Error connecting to DB: ", e);
@@ -48,82 +64,5 @@ db.connect((e) => {
         );
       }
     });
-  }
-});
-
-const upload = multer({ storage: multer.memoryStorage() });
-
-async function uploadToB2(file) {
-  const uploadParams = {
-    Bucket: process.env.bucket,
-    Key: `${Date.now()}_${file.originalname}`, // Unique file name
-    Body: file.buffer,
-    ContentType: file.mimetype,
-  };
-
-  const command = new PutObjectCommand(uploadParams);
-  await s3Client.send(command);
-}
-
-server.post("/uploadcsv", upload.single("file"), async (req, res) => {
-  const file = req.file;
-
-  const tableName = req.body?.tableName;
-
-  if (!file) {
-    return res.status(400).send("No file uploaded");
-  } else if (!tableName) {
-    return res.status(400).send("No table name entered");
-  }
-
-  try {
-    await uploadToB2(file);
-
-    const results = await handleParse(file.buffer);
-    const createQuery = process.env.createquery.replace(
-      "??",
-      mysql.escapeId(tableName)
-    );
-
-    let errors = [];
-
-    db.query(createQuery, (err, result) => {
-      if (err) {
-        errors.push(err);
-      }
-    });
-
-    for (const item of results) {
-      const query = `INSERT INTO ${mysql.escapeId(
-        tableName
-      )} (Date, Day_Of_Week, \
-        Officer, Officer_Phone_Num, Shotty, Shotty_Phone_Num,\
-         Driver, Driver_Phone_Num, Venmo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-      const values = [
-        item.Date,
-        item.Day,
-        item.Officer,
-        item.Officer_Phone_Num,
-        item.Shotty,
-        item.Shotty_Phone_Num,
-        item.Driver,
-        item.Driver_Phone_Num,
-        item.Driver_venmo,
-      ];
-
-      db.query(query, values, (err, result) => {
-        if (err) {
-          errors.push(err);
-        }
-      });
-    }
-
-    if (errors.length > 0) {
-      return res.status(500).send("Some queries failed");
-    }
-    res.send("File uploaded and data inserted");
-  } catch (err) {
-    console.error("Error parsing file: ", err);
-    res.status(500).send(err.message);
   }
 });
